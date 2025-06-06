c5c21c90e95cbf26f808a1b3ae0c0b95a5148e1d
Katsuraa <83263015+zhiif@users.noreply.github.com>
2025-06-06 19:24:17 +0700

    penambahan role pada database agar bisa memisahkan mana yang admin mana yang user

--- dbtokoa.sql
+++ dbtokoa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 05, 2025 at 05:07 PM
+-- Generation Time: Jun 05, 2025 at 05:08 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
